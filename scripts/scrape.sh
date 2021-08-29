51869e043a0cc6fd11c583a9eae7802b6c11e478
Dan Hicks <hicks.daniel.j@gmail.com>
2021-08-29 08:16:32 -0700

    incorporate automatic commits

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,4 +1,16 @@
 #! sh
 export PATH=/usr/local/bin/:$PATH
 Rscript 01_scrape.R
-osascript -e 'display notification "Ran covid dashboard scrape"'
+
+if [[ `git status --porcelain` ]]; then
+    # Changes
+    git add ../data/*
+    git commit -m "add new data"
+    echo "Changes"
+    osascript -e 'display notification "Scraped new data from UCM Covid dashboard"' 
+else
+    # No changes
+    echo "No changes"
+    osascript -e 'display notification "No new data on UCM Covid dashboard"'
+fi
+
